09fd75b64bf2210183e45831695d760a1e3fc8c3
ericliu <ericliu189@gmail.com>
2026-03-08 19:19:09 -0400

    Initial Commit

---
+++ Yard2Yum/Yard2YumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yard2YumApp.swift
+//  Yard2Yum
+//
+//  Created by Eric Liu on 08/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Yard2YumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
